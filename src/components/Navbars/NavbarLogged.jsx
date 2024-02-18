13b9ba181889d8c5b916a07aa444ea282480929c
alvaropeblas <121167838+alvaropeblas@users.noreply.github.com>
2024-02-18 13:17:57 +0100

    aa

--- src/components/Navbars/NavbarLogged.jsx
+++ src/components/Navbars/NavbarLogged.jsx
@@ -6,10 +6,9 @@ import LogOut from '../LogOut'
 const NavbarLogged = () => {
     return (
         <>
-            <div className='flex lg:flex-col flex-row items-center gap-12 text-white lg:h-[90vh] w-[90vw] py-[5em]'>
+            <div className='flex lg:flex-col flex-row items-center gap-16 text-white lg:h-[90vh] w-[90vw] py-[5em]'>
                 <Link to="/puzzles-front/"><img src={logo} alt="" className='lg:h-[100px] h-[100px] lg:w-[100px] w-[100px]' /></Link>
                 <Link to="/puzzles-front/menu" ><div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Menu</div></Link>
-                <div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Contacto</div>
                 <Link to="/puzzles-front/booking"><div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Calendario</div></Link>
                 <Link to="/puzzles-front/reservas"><div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Reservas</div></Link>
             </div>
--- src/components/Navbars/NavbarNotLogged.jsx
+++ src/components/Navbars/NavbarNotLogged.jsx
@@ -6,10 +6,9 @@ import Settings from '../Settings'
 const NavbarNotLogged = () => {
     return (
         <>
-            <div className='flex lg:flex-col flex-row items-center gap-8 text-white lg:h-[90vh] w-[90vw] py-[5em]'>
+            <div className='flex lg:flex-col flex-row items-center gap-14 text-white lg:h-[90vh] w-[90vw] py-[5em]'>
                 <Link to="/puzzles-front/"><img src={logo} alt="" className='lg:h-[100px] h-[100px]' /></Link>
                 <Link to="/puzzles-front/menu"><div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Menu</div></Link>
-                <div className='w-[5vw]  py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Contacto</div>
                 <Link to="/puzzles-front/booking"><div className='w-[5vw] py-[0.2em] flex justify-center m-2 hover:bg-BUTTON_COLOR rounded transition duration-300 cursor-pointer'>Calendario</div></Link>
             </div>
             <Settings />
